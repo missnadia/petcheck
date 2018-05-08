ddfd0529dc3e5daa84226ecc64a00a4d56f92746
Nadia Lee <miss.nadia.lee@gmail.com>
2018-05-08 02:35:56 +0000

    add code to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,44 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    !logged_in? ? (erb :"/users/create_user") : (redirect to "/pets")
+  end
+
+  post '/signup' do
+    if !logged_in?
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to "/signup"
+      else
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to "/pets"
+      end
+    else
+      redirect to "/signup"
+    end
+  end
+
+  get '/login' do
+    (!logged_in?) ? (erb :"/users/login") : (redirect to "/pets")
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/pets"
+    else
+      redirect to "/signup"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to "/login"
+    else
+      redirect to "/"
+    end
+  end
 end
