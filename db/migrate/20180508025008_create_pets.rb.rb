b04e906576b0a26dbbf89ebdb2aa6c26c2a25504
Nadia Lee <miss.nadia.lee@gmail.com>
2018-05-08 03:13:36 +0000

    add column info to pets table migration

--- db/migrate/20180508025008_create_pets.rb.rb
+++ db/migrate/20180508025008_create_pets.rb.rb
@@ -1,5 +1,15 @@
 class CreatePets.rb < ActiveRecord::Migration[5.2]
 
   def change
+    create_table :pets do |t|
+      t.string :name
+      t.date :birthday
+      t.boolean :fed
+      t.boolean :walk
+      t.boolean :dog_friendly
+      t.string :vet
+      t.text :add_notes
+      t.integer :user_id
+    end
   end
 end
