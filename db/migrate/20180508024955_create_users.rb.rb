1191413d07457a3cb903240f55d5deff77b8d2f2
Nadia Lee <miss.nadia.lee@gmail.com>
2018-05-08 02:54:30 +0000

    revise CreateUsers db

--- db/migrate/20180508024955_create_users.rb.rb
+++ db/migrate/20180508024955_create_users.rb.rb
@@ -1,5 +1,10 @@
 class CreateUsers.rb < ActiveRecord::Migration[5.2]
 
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+    end
   end
 end
